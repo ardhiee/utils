b86d2ef89e6925dbeebe78e3c448fb4c4872ddeb
Ardi Ismail <>
2019-11-25 15:10:12 +0700

    first commit

---
+++ utils.go
@@ -0,0 +1,35 @@
+package go-utils
+
+import (
+	"time"
+
+	"github.com/labstack/echo"
+	log "github.com/sirupsen/logrus"
+)
+
+type Logdata struct {
+	RequestID string
+}
+
+// MakeLogEntry using with logrus
+func MakeLogEntry(c echo.Context, l *Logdata) *log.Entry {
+
+	if c == nil && l == nil {
+		return log.WithFields(log.Fields{
+			"at": time.Now().Format("2006-01-02 15:04:05"),
+		})
+	} else if c == nil {
+		return log.WithFields(log.Fields{
+			"at":        time.Now().Format("2006-01-02 15:04:05"),
+			"requestid": l.RequestID,
+		})
+	}
+
+	return log.WithFields(log.Fields{
+		"requestid": c.Request().Header.Get("x-request-id"),
+		"at":        time.Now().Format("2006-01-02 15:04:05"),
+		"method":    c.Request().Method,
+		"uri":       c.Request().URL.String(),
+		"ip":        c.Request().RemoteAddr,
+	})
+}
